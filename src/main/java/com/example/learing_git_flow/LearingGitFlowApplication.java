3a1c9c807cdf1b47e04a0e88f8796d585a2e05dd
minhtan <tanpm@mpos.vn>
2021-06-18 11:30:42 +0700

    first commit

---
+++ src/main/java/com/example/learing_git_flow/LearingGitFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.learing_git_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearingGitFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearingGitFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learing_git_flow/LearingGitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learing_git_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearingGitFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
